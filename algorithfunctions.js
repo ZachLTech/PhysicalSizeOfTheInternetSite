4a7d772d9639f84734ffabd25e924bd9389b352a
ZachLTech <zlopezzl2021@gmail.com>
2023-04-09 19:55:30 -0400

    new file:   algorithfunctions.js 	new file:   algorithm.py 	new file:   index.html 	new file:   style.css

---
+++ algorithfunctions.js
@@ -0,0 +1,21 @@
+//According to some random source, the internet grows 43tb per second. (Don't know how long this statistic will last but who cares itll just be not completely accurate)
+//2.5HDD: 2.8in wide 4.0in long
+//3.5HDD: 
+
+let SizeTB = 9196800;
+let HDD35Stack14 = 14*3;
+let WidthInches = SizeTB*2.8;
+let WidthMiles = WidthInches*1.57828e-5;
+let LengthInches = SizeTB*4.0;
+let LenghtMiles = LengthInches*1.57828e-5;
+
+function addSizeTB(){
+    SizeTB += 43;
+}
+
+let UpdateInterval = setInterval(addSizeTB, 1000);
+let DocInterval = setInterval(Update, 1000);
+function Update() {
+    document.getElementById("SizeTB").innerHTML = SizeTB;
+}
+
