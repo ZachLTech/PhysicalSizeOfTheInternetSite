5e9fefe62bd3e6165b32459214d8d586662dcba5
ZachLTech <zlopezzl2021@gmail.com>
2023-04-22 21:52:30 -0400

    modified:   Stats/algorithfunctions.js

--- Stats/algorithfunctions.js
+++ Stats/algorithfunctions.js
@@ -6,7 +6,7 @@ let SizeTB = 106000000000;
 
         // Initialize the variable
         let stackHeight = 1;
-        let i = 0;
+        let i = 1;
         // Function to update the variable and display the value
         
         function updateVariable() {
@@ -14,7 +14,7 @@ let SizeTB = 106000000000;
             const inputValue = parseInt(document.getElementById("inputNumber").value);
             // Update the variable
             stackHeight = inputValue;
-            i = 0;
+            i = 1;
         }
         
 //65536 harddrives wide
@@ -53,10 +53,35 @@ function Width() {
     WidthHHDS += growth;
     let WidthInches = WidthHHDS*2.8;
     let WidthMiles = WidthInches*1.57828e-5;
+    for (i; i<stackHeight; i++){
+        WidthMiles = WidthMiles-(WidthMiles*(1/(i+1)));
+    }
     return WidthMiles;
 }
-let widthinterval = setInterval(Width, 333.33); 
+function WidthNoH() {
+    let HHDsNeeded = SizeTB/30.72;
+    let HHDsNeeded1b4 = (SizeTB-14)/30.72;
+    let growth = (HHDsNeeded-HHDsNeeded1b4)/2;
+    let WidthHHDS = 65536;
+    WidthHHDS += growth;
+    let WidthInches = WidthHHDS*2.8;
+    let WidthMiles = WidthInches*1.57828e-5;
+    return WidthMiles;
+}
 function Length() {
+    let HHDsNeeded = SizeTB/30.72;
+    let HHDsNeeded1b4 = (SizeTB-14)/30.72;
+    let growth = (HHDsNeeded-HHDsNeeded1b4)/2;
+    let LengthHHDS = 52650.76955159505;
+    LengthHHDS += growth;
+    let LengthInches = LengthHHDS*4.0;
+    let LengthMiles = LengthInches*1.57828e-5;
+    for (i; i<stackHeight; i++){
+        LengthMiles = LengthMiles-(LengthMiles*(1/(i+1)));
+    }
+    return LengthMiles;
+}
+function LengthNoH() {
     let HHDsNeeded = SizeTB/30.72;
     let HHDsNeeded1b4 = (SizeTB-14)/30.72;
     let growth = (HHDsNeeded-HHDsNeeded1b4)/2;
@@ -66,26 +91,64 @@ function Length() {
     let LengthMiles = LengthInches*1.57828e-5;
     return LengthMiles;
 }
-let lengthinterval = setInterval(Length, 333.33);
 
+function both(){
+    Width();
+    Length();
+}
+/**
 let layers = 1;
-
-function heightWidth(){
+let bothinterval= setInterval(both, 333.33)
+function WidthBasedOnHeight(){
     let WidthMiles = Width()-(Width()*(1/(layers+1)));
     layers+= 1;
     return WidthMiles;
 }  
-function heightLength(){
+function LengthBasedOnHeight(){
     let LengthMiles = Length()-(Length()*(1/(layers+1)));
     layers+= 1;
     return LengthMiles;
-}   
+}
+function Lengthmixingboth(){
+    let withboth = LengthBasedOnHeight+(LengthBasedOnHeight()-length())
+    return withboth
+}
+function Widthmixingboth(){
+    let withboth = (WidthBasedOnHeight()-Width())
+    return withboth
+}
+*/
 
+
+
+let lorw = 0
+function main(){
+    while(i < stackHeight){
+        if (lorw == 0){
+            WidthMi = Width();
+            lorw =1;
+        }
+        if (lorw == 1){
+            LengthMi = Length();
+            lorw = 0;
+        }
+
+        }
+    if(i==stackHeight){
+        LengthMi = LengthNoH();
+        WidthMi = WidthNoH();
+    }
+    }
+let maininterval = setInterval(main, 333.33)
+
+
+
+/** 
 function check(){
     
     while(i < stackHeight){
         if (Width() > Length()){
-            WidthMi = heightWidth();
+            WidthMi = heightWidth();30
         }
         if (Width() < Length()){
             LengthMi = heightLength();
@@ -94,6 +157,7 @@ function check(){
         }
     }
 let whileinterval = setInterval(check, 333.33);
+*/
 /** 
 function temporary(){
     console.log(heightLength());
