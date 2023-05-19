644a2421ed2bfb58864a251a56cbf1adbea76b9a
ZachLTech <zlopezzl2021@gmail.com>
2023-05-19 04:23:49 -0400

    modified:   Price/script.js 	modified:   Why/Index.html

--- Price/script.js
+++ Price/script.js
@@ -23,7 +23,7 @@ VanillaTilt.init(document.querySelectorAll(".social__links ul li"), {
 	
 	$('#arrowBtn').on('mouseenter', (e)=>{ gsap.to('.arrow', {y:10, duration:0.8, ease:'back.inOut(3)', overwrite:'auto'}); })
 	$('#arrowBtn').on('mouseleave', (e)=>{ gsap.to('.arrow', {y:0, duration:0.5, ease:'power3.out', overwrite:'auto'}); })
-	$('#arrowBtn').on('click', (e)=>{ gsap.to(window, {scrollTo:innerHeight, duration:1.5, ease:'power1.inOut'}); }) // scrollTo requires the ScrollTo plugin (not to be confused w/ ScrollTrigger)
+	$('#arrowBtn').on('click', (e)=>{ gsap.to(window, {scrollTo:innerHeight+1010, duration:2.5, ease:'power1.inOut'}); }) // scrollTo requires the ScrollTo plugin (not to be confused w/ ScrollTrigger)
 
 
 
