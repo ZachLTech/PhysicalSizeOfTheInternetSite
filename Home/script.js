8f104128ba8fdd484cabe0b9b272582b845de693
ZachLTech <zlopezzl2021@gmail.com>
2023-06-06 03:36:37 -0400

    modified:   Home/script.js 	modified:   Home/style.css 	new file:   assets/wave-haikei.png 	modified:   index.html

---
+++ Home/script.js
@@ -0,0 +1,11 @@
+const header = document.getElementById("headerSlideIn");
+const toggleClass = "stickyHeader";
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > 150) {
+    header.classList.add(toggleClass);
+  } else {
+    header.classList.remove(toggleClass);
+  }
+});
